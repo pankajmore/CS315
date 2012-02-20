d582ae7b85d485a4b71f9b074698a79a51864d23
Pankaj More <pankajmore@gmail.com>
2012-02-20 10:13:26 +0530

    administrative stuff

---
+++ Assignment2/setup.py
@@ -0,0 +1,16 @@
+import os, sys
+from distutils.core import setup
+
+
+setup(
+    # metadata
+    name='Assignment2',
+    description='CS315 Assignment2 ',
+    install_requires=['oursql'],
+    license='BSD',
+    version='0.01',
+    authors = ['Pankaj More'],
+    author_email='pankajm@iitk.ac.in',
+    url='https://github.com/pankajmore/CS315',
+    package=['assignment2'],
+)
